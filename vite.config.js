e4abb2c13c650c6f3403dd07b60eedb8a5325b43
Sahansa Fernando <sahansaf2@gmail.com>
2026-02-08 21:26:46 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/estate-agent-app/',
   plugins: [react()],
   test: {
     globals: true,
